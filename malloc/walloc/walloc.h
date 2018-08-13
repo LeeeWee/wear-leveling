be3bba5500cbfd283e72dbc1deed35e8dd13de51
lw <541602953@qq.com>
2018-08-14 00:46:31 +0800

    walloc.h

---
+++ malloc/walloc/walloc.h
@@ -0,0 +1,20 @@
+#ifndef _WALLOC_H
+#define _WALLOC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void walloc_init();
+void walloc_exit(void);
+void *walloc(unsigned size);
+int wfree(void *addr);
+void walloc_print(void);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ tools/test/test2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // printf("....\n");
+    printf("sizeof(int): %d\n", sizeof(int));
+    printf("sizeof(long): %d\n", sizeof(long));
+    printf("sizeof(long long): %d\n", sizeof(long long));
+    printf("sizeof(float): %d\n", sizeof(float));
+    printf("sizeof(double): %d\n", sizeof(double));
+    return 0;
+}
